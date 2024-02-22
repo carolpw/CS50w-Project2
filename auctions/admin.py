d8bf5c44c7cff545b2932d849c380a30bf7b3540
Caro López <lopezolea.c@gmail.com>
2024-02-22 14:34:27 +0100

    Added models (Category, Listing, Bids and Comments) and registered them in admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User, Listing, Bids, Comments, Category
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Listing)
+admin.site.register(Bids)
+admin.site.register(Comments)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,23 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=16)
+
+class Listing(models.Model):
+    name = models.CharField(max_length=32)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    description = models.CharField(max_length=256)
+    price = models.FloatField()
+    date =  models.DateField(auto_now=True)
+    image = models.CharField(max_length=256) #ImageField, install Pillow?
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="category")
+
+class Bids(models.Model):
+    pass
+
+class Comments(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    text = models.CharField(max_length=256)
+
