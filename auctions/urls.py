ea76e27f4360529a54932081a81718e7b2b76d51
Caro López <lopezolea.c@gmail.com>
2024-03-10 19:21:49 +0100

    Added path and view for 'Create Listing', 'Watchlist' and 'Categories'. Also added them in the menu.

--- auctions/urls.py
+++ auctions/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("createListing", views.create_listing, name="create_listing"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("categories", views.categories, name="categories")
 ]
--- auctions/views.py
+++ auctions/views.py
@@ -61,3 +61,14 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    return render(request, "auctions/createListing.html")
+
+
+def watchlist(request):
+    return render(request, "auctions/watchlist.html")
+
+def categories(request):
+    return render(request, "auctions/categories.html")
